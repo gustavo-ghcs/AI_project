f41223e480449a736bbd408db717a53c14ab086a
Gustavo Hollanda <104395661+gustavo-ghcs@users.noreply.github.com>
2025-03-24 18:49:34 -0300

    enhance classificate.py to include color mapping for predictions and improve image resolution

--- src/classificate.py
+++ src/classificate.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pickle
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
@@ -40,21 +41,31 @@ new_pcd['Classification'] = predictions
 
 # Salvar a nuvem classificada
 print("Salvando nuvem classificada")
-output_file = data_folder + "nova_nuvem_classificada.xyz"
+output_file = data_folder + "nuvem_classificada.xyz"
 new_pcd.to_csv(output_file, sep=' ', index=False, header=True)
 
 print(f"Nuvem classificada salva em: {output_file}")
 
+# Mapeando cores para as classes
+color_map = {
+    1: "#0009DE",  # Terreno (ground)
+    2: "#7EFF0C",  # Vegetação (vegetation)
+    3: "#FB0304"   # Edifícios (buildings)
+}
+
+# Converter previsões para cores
+colors = np.array([color_map[class_id] for class_id in predictions])
+
 # Gerar imagem da Nuvem de Pontos Classificada
 print("Gerando imagem da Nuvem de Pontos Classificada")
 fig, ax = plt.subplots(figsize=(6, 6))
-scatter = ax.scatter(new_pcd['X'], new_pcd['Y'], c=predictions, cmap="viridis", s=0.05)
+ax.scatter(new_pcd['X'], new_pcd['Y'], c=colors, s=0.05)
 ax.set_title('3D Point Cloud Predictions')
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
 
 # Salvar imagem
-plt.savefig(output_folder + "nuvem_classificada.jpg", dpi=300)
+plt.savefig(output_folder + "nuvem_classificada.jpg", dpi=600)
 print(f"Nuvem de Pontos Classificada salva em: {output_folder}nuvem_classificada.jpg")
 
 print("Processo concluído com sucesso!")
